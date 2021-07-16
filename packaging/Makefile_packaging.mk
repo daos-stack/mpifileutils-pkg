ca2ba8ec009866906e72fe68dcf443c3717caa82
Danielle Sikich <danielle.sikich@intel.com>
2021-07-16 17:32:48 +0000

    use dnf to install
    Signed-off-by: Danielle Sikich <danielle.sikich@intel.com>

--- packaging/Makefile_packaging.mk
+++ packaging/Makefile_packaging.mk
@@ -393,7 +393,7 @@ endif
 test:
 	# Test the rpmbuild by installing the built RPM
 	$(call install_repos,$(REPO_NAME)@$(BRANCH_NAME):$(BUILD_NUMBER))
-	yum -y install $(TEST_PACKAGES)
+	dnf -y install $(TEST_PACKAGES)
 
 show_spec:
 	@echo '$(SPEC)'
